15f2c1d73600f709da24b2c2f121237d6a96f261
Henrique Nunes <79033028+riquen@users.noreply.github.com>
2022-07-21 13:09:14 -0300

    finishing pomodoro

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useInterval } from '../../hooks/useInterval';
+import { secondsToTime } from '../../utils/functions/secondsToTime';
 import { Button } from '../Button';
 import { Timer } from '../Timer';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellStart = require('../../sounds/src_sounds_bell-start.mp3');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellFinish = require('../../sounds/src_sounds_bell-finish.mp3');
+
+const audioStartWorking = new Audio(bellStart);
+const audioFinishWorking = new Audio(bellFinish);
 
 interface Props {
   pomodoroTime: number;
@@ -11,30 +20,91 @@ interface Props {
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
+  const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
+  const [cyclesQttManager, setCyclesQttManager] = useState(
+    new Array(props.cycles - 1).fill(true),
+  );
+  const [completedCycles, setCompletedCycles] = useState(0);
+  const [fullWorkingTime, setFullWorkingTime] = useState(0);
+  const [numberOfPomodoros, setNumberOfPomodoros] = useState(0);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  const configureWork = useCallback(() => {
+    setTimeCounting(true);
+    setWorking(true);
+    setResting(false);
+    setMainTime(props.pomodoroTime);
+    audioStartWorking.play();
+  }, [props.pomodoroTime]);
+
+  const configureRest = useCallback(
+    (long: boolean) => {
+      setTimeCounting(true);
+      setWorking(false);
+      setResting(true);
+      long ? setMainTime(props.longRestTime) : setMainTime(props.shortRestTime);
+      audioFinishWorking.play();
+    },
+    [props.longRestTime, props.shortRestTime],
+  );
+
+  useEffect(() => {
+    working && document.body.classList.add('working');
+    resting && document.body.classList.remove('working');
+    if (mainTime > 0) return;
+
+    if (working && cyclesQttManager.length > 0) {
+      configureRest(false);
+      cyclesQttManager.pop();
+    } else {
+      configureRest(true);
+      setCyclesQttManager(new Array(props.cycles - 1).fill(true));
+      setCompletedCycles(completedCycles + 1);
+    }
+
+    working && setNumberOfPomodoros(numberOfPomodoros + 1);
+    resting && configureWork();
+  }, [
+    completedCycles,
+    configureRest,
+    configureWork,
+    cyclesQttManager,
+    mainTime,
+    numberOfPomodoros,
+    props.cycles,
+    resting,
+    working,
+  ]);
+
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+      working && setFullWorkingTime(fullWorkingTime + 1);
+    },
+    timeCounting ? 1000 : null,
+  );
 
   return (
     <div className="pomodoro">
-      <h2>You are: working</h2>
+      <h2>Você está {working ? 'trabalhando' : 'descansando'}</h2>
       <Timer mainTime={mainTime} />
 
       <div className="controls">
-        <Button text="teste" onClick={() => console.log('clicou')} />
-        <Button text="teste" onClick={() => console.log('clicou')} />
-        <Button text="teste" onClick={() => console.log('clicou')} />
+        <Button text="Work" onClick={() => configureWork()} />
+        <Button text="Rest" onClick={() => configureRest(false)} />
+        <Button
+          className={!working && !resting ? 'hidden' : ''}
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        />
       </div>
 
       <div className="details">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-          sint accusamus quae optio, doloribus ea earum. Qui quis assumenda,
-          aspernatur unde suscipit at harum velit. Sed, saepe. Sint, eaque
-          vitae!
-        </p>
+        <p>Ciclos concluídos: {completedCycles}</p>
+        <p>Horas trabalhadas: {secondsToTime(fullWorkingTime)}</p>
+        <p>Pomodoros concluídos: {numberOfPomodoros}</p>
       </div>
     </div>
   );
